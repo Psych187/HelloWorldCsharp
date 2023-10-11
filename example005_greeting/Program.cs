4c10a349d2dbf0320d4607dd4c00b5d430a7051e
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-11 14:11:07 +0300

    example005 greeting added

---
+++ example005_greeting/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray, Masha!");
+}
+else {
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
